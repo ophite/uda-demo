62e4e355b1d63add9739f9b730f189bde4b26f1a
yurakobernik <yurii.k@maxitechs.com>
2019-07-07 09:17:51 +0300

    1st commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default () => (
   <div style={{ textAlign: 'center' }}>
-    <h1>Welcome to React-Static Test2</h1>
+    <h1>Welcome to React-Static Test211</h1>
   </div>
 )
