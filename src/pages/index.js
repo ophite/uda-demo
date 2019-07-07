45c01a37498bf7ff61155604de55ab0c7df72888
yurakobernik <yurii.k@maxitechs.com>
2019-07-07 09:41:31 +0300

    dev

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 
 export default () => (
   <div style={{ textAlign: 'center' }}>
-    <h1>Welcome to React-Static Test2</h1>
+    <Title>Welcome to React-Static Test2</Title>
   </div>
 )
