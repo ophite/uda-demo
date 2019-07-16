129e92a7739e0c79afc2180aeccc2380a717edf2
yurakobernik <yurii.k@maxitechs.com>
2019-07-16 07:56:26 +0300

    Squashed commit of the following:
    commit f89978c84b1ebb794be301a39bf5518459df9429
    Author: yurakobernik <yurii.k@maxitechs.com>
    Date:   Sun Jul 7 12:19:37 2019 +0300
    
        dev
    
    commit f778f3363278ef5bca9b02ab5d3fbfbca6d32265
    Author: yurakobernik <yurii.k@maxitechs.com>
    Date:   Sun Jul 7 09:45:03 2019 +0300
    
        dev
    
    commit 45c01a37498bf7ff61155604de55ab0c7df72888
    Author: yurakobernik <yurii.k@maxitechs.com>
    Date:   Sun Jul 7 09:41:31 2019 +0300
    
        dev

--- src/pages/marketing-tools.js
+++ src/pages/marketing-tools.js
@@ -1,5 +1,86 @@
 import React from 'react';
 import Helmet from 'components/Helmet.component';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
+import Image from 'react-bootstrap/Image';
+import Container from 'react-bootstrap/Container';
+import { smmListData } from '../data/smmList.data';
+
+
+const SmmList = () => {
+  const columns = [
+    {
+      Header: 'Site',
+      accessor: 'site',
+      sortable: false,
+      Cell: cellProps => <a href={ cellProps.value }>{cellProps.value}</a>,
+    },
+    {
+      Header: 'Logo',
+      accessor: 'logo',
+      sortable: false,
+      Cell: cellProps => <Image src={ cellProps.value } />,
+    },
+    {
+      Header: 'Name',
+      accessor: 'name',
+      sortable: false,
+    },
+    {
+      Header: 'Description',
+      accessor: 'description',
+      sortable: false,
+    },
+    {
+      Header: 'Page (Get now)',
+      accessor: 'page',
+      sortable: false,
+      Cell: cellProps => <a href={ cellProps.value }>{cellProps.value}</a>,
+    },
+    {
+      Header: 'Quantity',
+      accessor: 'quantity',
+      sortable: false,
+    },
+    {
+      Header: 'Frequency',
+      accessor: 'frequency',
+      sortable: false,
+    },
+    {
+      id: 'timeDeliveryId',
+      Header: 'Time Delivery',
+      accessor: d => d.timeDeliveryValue,
+      Cell: (cellProps) => {
+        return (<div>{cellProps.original.timeDelivery}</div>);
+      },
+      sortMethod: (a, b) => {
+        if (a === b) {
+          return a > b ? 1 : -1;
+        }
+        return a > b ? 1 : -1;
+      },
+      sortable: true,
+    },
+    {
+      Header: 'Email',
+      accessor: 'email',
+      sortable: false,
+    },
+    {
+      Header: 'Overall Rating',
+      accessor: 'rating',
+      sortable: true,
+    },
+  ];
+
+  return (
+    <ReactTable
+      data={ smmListData }
+      columns={ columns }
+    />
+  );
+};
 
 export default () => (
   <React.Fragment>
@@ -18,6 +99,10 @@ export default () => (
       twitterTitle='Marketing-tools Home Social Media Management Tools Listing'
       twitterImage='Marketing-tools Home image path'
     />
-    <p>Marketing tools.</p>
+    <Container>
+      <p>Marketing tools.</p>
+      <div>SMM table list</div>
+      <SmmList />
+    </Container>
   </React.Fragment>
 );
