767475c63ad8acca3b183121d3c68e2b81ec8536
dmitry <dmitry.a@maxitechs.com>
2019-08-14 21:21:20 +0300

    remove css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Root, Routes, addPrefetchExcludes } from 'react-static';
-//
+
 import { Router } from 'components/Router';
 import Dynamic from 'containers/Dynamic';
 import HeaderMenu from 'components/HeaderMenu.component';
 import Footer from 'components/Footer.component';
 import styled, { createGlobalStyle } from 'styled-components';
-import './app--end.css';
-import './app.css';
 import './main-theme.css';
+import './app--end.css';
 
 
 // Any routes that start with 'dynamic' will be treated as non-static routes
@@ -23,6 +22,30 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
   }
+  * {
+  scroll-behavior: smooth;
+  }
+  
+  body {
+    font-family: 'HelveticaNeue-Light', 'Helvetica Neue Light', 'Helvetica Neue',
+      Helvetica, Arial, 'Lucida Grande', sans-serif;
+    font-weight: 300;
+    font-size: 16px;
+    margin: 0;
+    padding: 0;
+  
+  }
+  
+  a {
+    text-decoration: none;
+    color: #108db8;
+    font-weight: bold;
+  }
+  
+  img {
+    max-width: 100%;
+  }
+
 `;
 
 const AppStyles = styled.div`
@@ -33,19 +56,19 @@ const AppStyles = styled.div`
 function App() {
   return (
     <Root>
-        <AppStyles>
-          <GlobalStyle/>
-          <HeaderMenu/>
-          <div className='content'>
-            <React.Suspense fallback={<em>Loading...</em>}>
-              <Router>
-                <Dynamic path='dynamic'/>
-                <Routes path='*'/>
-              </Router>
-            </React.Suspense>
-          </div>
-          <Footer/>
-        </AppStyles>
+      <AppStyles>
+        <GlobalStyle />
+        <HeaderMenu />
+        <div className='content'>
+          <React.Suspense fallback={ <em>Loading...</em> }>
+            <Router>
+              <Dynamic path='dynamic' />
+              <Routes path='*' />
+            </Router>
+          </React.Suspense>
+        </div>
+        <Footer />
+      </AppStyles>
     </Root>
 
   );
